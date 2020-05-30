6356d34555d2fa6c7880aea5554abcba0e4f8a09
unknown <mr.azik98@mail.ru>
2020-05-30 20:01:04 +0500

    html/css about 50% done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import './styles/main.scss';
+import './styles/main.scss'; 
 
+// Is this nice? GOOD JOB!!! You are senior developer! :) ☺ 
 const App = () => {
 	return (
 		<div className="layout">
-			<div className="layout__rectange">Content here...</div>
+			<div className="layout__rectange">
+				<table className="table">
+					<tr>
+						<td><p>Calculation buffer</p></td>
+					</tr>
+					<tr>
+						<td><input type="input" value="text"/></td>
+					</tr>
+					<tr>
+						<td><input type="button" value="C"/></td>
+					</tr>
+					<tr>
+						<table className="button-table">
+							<tr>
+								<td><input type="button" value="1"/></td>
+								<td><input type="button" value="2"/></td>
+								<td><input type="button" value="3"/></td>
+								<td><input type="button" value="/"/></td>
+							</tr>
+							<tr>
+								<td><input type="button" value="4"/></td>
+								<td><input type="button" value="5"/></td>
+								<td><input type="button" value="6"/></td>
+								<td><input type="button" value="×"/></td>
+							</tr>
+							<tr>
+								<td><input type="button" value="7"/></td>
+								<td><input type="button" value="8"/></td>
+								<td><input type="button" value="9"/></td>
+								<td><input type="button" value="+"/></td>
+							</tr>
+							<tr>
+								<td><input type="button" value="." /></td> 
+								<td><input type="button" value="0"/></td> 
+								<td><input type="button" value="="/></td> 
+								<td><input type="button" value="*"/></td> 
+							</tr>
+						</table>
+					</tr>
+				</table>
+			</div>
 		</div>
 	);
 };
