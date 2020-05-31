9cffed7b619ace796ad219ed65b4cc67d95cc2d3
unknown <mr.azik98@mail.ru>
2020-05-31 14:16:50 +0500

    spacing method changed

--- src/index.js
+++ src/index.js
@@ -5,51 +5,51 @@ import './styles/main.scss';
 
 // Is this nice? GOOD JOB!!! You are senior developer! :) ☺ 
 const App = () => {
-	return (
-		<div className="layout">
-			<div className="layout__rectange">
-				<table className="table">
-					<tr>
-						<td><p>Calculation buffer</p></td>
-					</tr>
-					<tr>
-						<td><input type="input" value="text"/></td>
-					</tr>
-					<tr>
-						<td><input type="button" value="C"/></td>
-					</tr>
-					<tr>
-						<table className="button-table">
-							<tr>
-								<td><input type="button" value="1"/></td>
-								<td><input type="button" value="2"/></td>
-								<td><input type="button" value="3"/></td>
-								<td><input type="button" value="/"/></td>
-							</tr>
-							<tr>
-								<td><input type="button" value="4"/></td>
-								<td><input type="button" value="5"/></td>
-								<td><input type="button" value="6"/></td>
-								<td><input type="button" value="×"/></td>
-							</tr>
-							<tr>
-								<td><input type="button" value="7"/></td>
-								<td><input type="button" value="8"/></td>
-								<td><input type="button" value="9"/></td>
-								<td><input type="button" value="+"/></td>
-							</tr>
-							<tr>
-								<td><input type="button" value="." /></td> 
-								<td><input type="button" value="0"/></td> 
-								<td><input type="button" value="="/></td> 
-								<td><input type="button" value="*"/></td> 
-							</tr>
-						</table>
-					</tr>
-				</table>
-			</div>
-		</div>
-	);
+  return (
+    <div className="layout">
+      <div className="layout__rectange">
+        <table className="table">
+          <tr>
+            <td><p>Calculation buffer</p></td>
+          </tr>
+          <tr>
+            <td><input type="input" value="text"/></td>
+          </tr>
+          <tr>
+            <td><input type="button" value="C"/></td>
+          </tr>
+          <tr>
+            <table className="button-table">
+              <tr>
+                <td><input type="button" value="1"/></td>
+                <td><input type="button" value="2"/></td>
+                <td><input type="button" value="3"/></td>
+                <td><input type="button" value="/"/></td>
+              </tr>
+              <tr>
+                <td><input type="button" value="4"/></td>
+                <td><input type="button" value="5"/></td>
+                <td><input type="button" value="6"/></td>
+                <td><input type="button" value="×"/></td>
+              </tr>
+              <tr>
+                <td><input type="button" value="7"/></td>
+                <td><input type="button" value="8"/></td>
+                <td><input type="button" value="9"/></td>
+                <td><input type="button" value="+"/></td>
+              </tr>
+              <tr>
+                <td><input type="button" value="." /></td> 
+                <td><input type="button" value="0"/></td> 
+                <td><input type="button" value="="/></td> 
+                <td><input type="button" value="*"/></td> 
+              </tr>
+            </table>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
