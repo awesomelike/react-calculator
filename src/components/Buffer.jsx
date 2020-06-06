7aea3ae322dfb2ead0ff5125322f306d4033d5c4
unknown <mr.azik98@mail.ru>
2020-06-06 11:20:14 +0500

    updated React Spring version

--- src/components/Buffer.jsx
+++ src/components/Buffer.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Spring } from 'react-spring';
+import { useSpring, animated } from 'react-spring';
 import PropTypes from 'prop-types';
 
-const Buffer = ({ buffer }) => (
-  <td>
-    <Spring
-      from={{ opacity: 0, marginTop: -50 }}
-      to={{ opacity: 1, marginTop: -20 }}
-      config={{ duration: 250 }}
-    >
-      {(props) => (
-        <p className="buffer" style={props} key={buffer}>
-          {buffer}
-        </p>
-      )}
-    </Spring>
-  </td>
-);
+const Buffer = ({ buffer }) => {
+  const props = useSpring({
+    from: { opacity: 0, marginTop: -50 },
+    to: [{ opacity: 1, marginTop: -20 }],
+    config: { duration: 250 },
+  });
+  return <td><animated.p className="buffer" style={props}>{buffer}</animated.p></td>;
+};
 
 Buffer.defaultProps = {
   buffer: '',
